4617f3601a2c6e8d5e52386741c96c99551d9b54
Lucije <55711219+Lucije@users.noreply.github.com>
2022-01-18 11:17:27 +0100

    Add files via upload

---
+++ js/veterina.js
@@ -0,0 +1,24 @@
+const slider = document.getElementById("slider");
+const buttons = document.getElementById("boxes");
+let last, now;
+
+slider.oninput = function() {
+    buttons.style.marginLeft =  -slider.value * 0.175 + 0.56 + "rem";
+}
+
+buttons.addEventListener('touchmove', function(event) {
+    now = event.changedTouches[0].screenX;
+
+    slider.value = parseInt(slider.value) + last - now;
+
+    if(last > now) {
+        if(last - now > 100 - slider.value) slider.value = 100;
+    }
+    if(now > last) {
+        if(now - last > slider.value) slider.value = 0;
+    }
+
+    buttons.style.marginLeft = -slider.value * 0.175 + 0.56 + "rem";
+
+    last = now;
+});
